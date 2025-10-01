bb9e8281f0b5e83b39d65f147fd554cfa02f4a1f
Jesse Braide <jessinspired.tech@gmail.com>
2025-10-01 08:35:31 +0200

    added images to browse images

--- _base/listings/serializers.py
+++ _base/listings/serializers.py
@@ -10,8 +10,44 @@ from .models import (
     LandImage,
 )
 
+# media files serializers
 
+
+class GenericImageSerializer(serializers.ModelSerializer):
+    url = serializers.SerializerMethodField()
+
+    class Meta:
+        fields = ["id", "url"]
+
+    def get_url(self, obj):
+        return obj.image.url
+
+
+class ApartmentForRentImageSerializer(GenericImageSerializer):
+    class Meta:
+        model = ApartmentForRentImage
+        fields = ["id", "image", "apartment_for_rent", "uploaded_at"]
+        read_only_fields = ["id", "uploaded_at"]
+
+
+class ApartmentForSaleImageSerializer(GenericImageSerializer):
+    class Meta:
+        model = ApartmentForSaleImage
+        fields = ["id", "image", "apartment_for_sale", "uploaded_at"]
+        read_only_fields = ["id", "uploaded_at"]
+
+
+class LandImageSerializer(GenericImageSerializer):
+    class Meta:
+        model = LandImage
+        fields = ["id", "image", "land", "uploaded_at"]
+        read_only_fields = ["id", "uploaded_at"]
+
+
+# property serializers
 class ApartmentForSaleSerializer(serializers.ModelSerializer):
+    images = ApartmentForSaleImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = ApartmentForSale
         fields = "__all__"
@@ -19,6 +55,8 @@ class ApartmentForSaleSerializer(serializers.ModelSerializer):
 
 
 class ApartmentForRentSerializer(serializers.ModelSerializer):
+    images = ApartmentForRentImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = ApartmentForRent
         fields = "__all__"
@@ -26,13 +64,15 @@ class ApartmentForRentSerializer(serializers.ModelSerializer):
 
 
 class LandSerializer(serializers.ModelSerializer):
+    images = LandImageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Land
         fields = "__all__"
         read_only_fields = ["created_at", "updated_at"]
 
 
-# Listing images serializers
+# for demo
 class PropertyImageSerializer(serializers.ModelSerializer):
     # image_url = serializers.SerializerMethodField()
 
@@ -46,24 +86,3 @@ class PropertyImageSerializer(serializers.ModelSerializer):
     #         # This uses Django Storage backend (CloudFront if set)
     #         return obj.image.url
     #     return None
-
-
-class ApartmentForRentImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ApartmentForRentImage
-        fields = ["id", "image", "apartment_for_rent", "uploaded_at"]
-        read_only_fields = ["id", "uploaded_at"]
-
-
-class ApartmentForSaleImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ApartmentForSaleImage
-        fields = ["id", "image", "apartment_for_sale", "uploaded_at"]
-        read_only_fields = ["id", "uploaded_at"]
-
-
-class LandImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LandImage
-        fields = ["id", "image", "land", "uploaded_at"]
-        read_only_fields = ["id", "uploaded_at"]
